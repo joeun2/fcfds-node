ce518d8953e2124dd7633ea3e12b919d4109ab76
DESKTOP-JAOS7MI\Joeun <joeun2312@gmail.com>
2016-11-21 16:30:10 +0900

    add home route: add home route in app.js print request.url at console

--- firstnodeserver/app.js
+++ firstnodeserver/app.js
@@ -3,9 +3,15 @@ var http = require('http');
 
 //서버
 http.createServer(function(request, response){
-	response.writeHead(200,{'Content-Type':'text/plain'});//값에메타정보
-	response.write('Hello FDS!!!!!');//원하는것을 작성
-	response.end();//응답멈춰줌
+	// response.writeHead(200,{'Content-Type':'text/plain'});//값에메타정보 //생략가능
+	// response.write('Hello FDS!!!!!');//원하는것을 작성
+	// response.end();//응답멈춰줌
+
+	if (request.url === "/") {
+		console.log(request.url);//request해서 url을 받음
+		response.write('This is HOME!!!!');//plain text로 사용하는것이 좋음(<p></p>태그쓰는것은 안좋음)
+		response.end();
+	}
 }).listen(process.env.PORT || 3030);
 //server을 만듬//listen은 포트번호를 가지고 있다 그것을3030으로 바꿔줌
 
